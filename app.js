c29501bb8e83664bbc6e2398ac15bc8847d78059
Christian Lisangola <jochri3@gmail.com>
2022-09-29 12:44:23 +0300

    tempate strings

---
+++ app.js
@@ -0,0 +1,23 @@
+let nombre1 = 45;
+let nombre2 = 55;
+let somme = nombre1 + nombre2;
+// console.log(somme);
+console.log(nombre1 + " + " + nombre2 + " = " + somme);
+
+// Template strings
+console.log(`${nombre1} + ${nombre2} = ${somme}`);
+
+// Affichage sans template strings
+console.log(
+  "Je suis\nTu es\nElle/Il est\nNous sommes\nVous êtes\nElles/Ils sont"
+);
+
+// Affichage avec template strings
+console.log(`
+Je suis
+Tu es
+Elle/Il est
+Nous sommes
+Vous êtes
+Elles/Ils sont
+`);
