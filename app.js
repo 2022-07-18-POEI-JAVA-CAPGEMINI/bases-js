718a84ea8e1516b217cd085e59a93bbfd54afc90
Christian Lisangola <jochri3@gmail.com>
2022-09-29 16:59:44 +0300

    add loop

--- app.js
+++ app.js
@@ -1,32 +1,64 @@
-let age = 17;
-
-if (age >= 18) {
-  console.log("Grand garçon!!!");
-} else {
-  console.log("Petit garçon!!!");
+// Boucle for
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
 }
 
-// Ici, on ne compare que les valeurs, et non les types
-// Pas recommandée
-if (age == "17") {
-  console.log("Ouii");
-}
+// Boucle while : v1
+// const codePin = parseInt(prompt("Définir un code PIN: "));
 
-// Ici,  il s'agit d'une égalité stricte.Parce qu'on compare les types
-// et les valeurs
-if (age === "17") {
-  console.log("Noonnn");
-}
+// let codePinATester;
+// codePIN : 1234
+// codePinATester : 442
+// while (codePinATester !== codePin) {
+//   codePinATester = parseInt(
+//     prompt("Veuillez entrer un code pour débloquer l'appareil : ")
+//   );
+//   if (codePinATester !== codePin) {
+//     alert("Code PIN erroné, veuillez recommencer.");
+//   } else {
+//     alert("Bienvenue!!!!!!!");
+//   }
+// }
 
-/**
- * Opérateurs de comparaison & logiques
- *   == : égalité
- *   === : égalité stricte
- *   >= : Supérieur ou égal
- *  <= : Inférieur ou égal
- *   != : Différent
- *   !== : Différence strict
- *   && : ET/AND
- *   || : OU/OR
- *   ! 
- */
+//1. Limiter les essais
+// 2. Entrer que des chiffres
+// 3. Duplication du test(Faire un premier test en dehors de la boucle)
+// Boucle while : v1
+// const codePin = parseInt(prompt("Définir un code PIN: "));
+
+// let codePinATester;
+// while (true) {
+//   codePinATester = parseInt(
+//     prompt("Veuillez entrer un code pour débloquer l'appareil : ")
+//   );
+//   if (codePinATester !== codePin) {
+//     alert("Code PIN erroné, veuillez recommencer.");
+//   } else {
+//     alert("Bienvenue!!!!!!!");
+//     break;
+//   }
+// }
+
+// v3
+const codePin = parseInt(prompt("Définir un code PIN: "));
+let nombreEssais = 3;
+
+let codePinATester;
+while (true) {
+  codePinATester = parseInt(
+    prompt("Veuillez entrer un code pour débloquer l'appareil : ")
+  );
+  nombreEssais--;
+  if (codePinATester === codePin) {
+    alert("Bienvenue!!!!!!!");
+    break;
+  }
+  if (nombreEssais === 0) {
+    alert(
+      "Téléphone bloqué, car vous avez entrez 3 fois de mauvaises valeurs."
+    );
+    break;
+  }
+  alert("Code PIN erroné, veuillez recommencer.");
+  alert(`${nombreEssais} tentative(s) restante(s).`);
+}
