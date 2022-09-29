67fcec35e11fa103dae1edfc769f0f71fb591aa6
Christian Lisangola <jochri3@gmail.com>
2022-09-29 12:27:46 +0300

    var, let et const

---
+++ let_const.js
@@ -0,0 +1,14 @@
+// Déclaration des variables
+let nom = "Lisangola";
+nom = "Jean";
+nom = "Luc";
+let pays = "France";
+
+console.log(nom);
+console.log(pays);
+
+// Déclaration des constantes
+const AGE_RETRAITE = 90;
+AGE_RETRAITE = 60; // Erreur car AGE_RETRAITE est une constante
+
+
---
+++ var.js
@@ -0,0 +1,20 @@
+// Mauvaises pratiques
+
+// Cas 1 : Hoisting
+console.log("Pays : ", pays);
+var pays = "France";
+
+// Accède à i en dehors de sa portée
+
+// Cas 2
+for (var i = 1; i <= 10; i++) {
+  console.log("Dans la boucle : ", i);
+}
+console.log("En dehors la boucle : ", i); //problème
+
+// Cas 3
+if (true) {
+  let pays = "France";
+  console.log("Pays(dans IF) : ", pays);
+}
+console.log("Pays(En dehors de IF) : ", pays); //problème
