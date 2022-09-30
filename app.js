9b6cc8daef9ed2c0680b786c9d67126b192101c4
Christian Lisangola <jochri3@gmail.com>
2022-09-30 10:34:45 +0300

    add arrow functions

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
-// Addtion 2 nombres
 function addition(nombre1, nombre2) {
   return nombre1 + nombre2;
 }
 
-const addition2 = function (nombre1, nombre2) {
+// = > : Arrow function
+// - > : fonction lambda
+const addition2 = (nombre1, nombre2) => {
   return nombre1 + nombre2;
 };
 
-console.log(addition(5, 6));
-console.log(addition2(11, 12));
-
-function calculerLaSommeDuTableau(tableau) {
-  let somme = 0;
-  for (let i = 0; i < tableau.length; i++) {
-    somme += tableau[i];
-  }
-  return somme;
-}
-
-const monTableau = [2, 3, 4, 5, 6];
-console.log(calculerLaSommeDuTableau(monTableau));
+const addition3 = (nombre1, nombre2) => nombre1 + nombre2;
