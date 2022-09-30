952f6646ba14cdce95ed40a6a379cd701308c2b2
Christian Lisangola <jochri3@gmail.com>
2022-09-30 10:28:38 +0300

    add functions

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
-// Création d'un tableau vide
-const monTableau = [];
-monTableau.push(1);
-monTableau.push(2);
-// const monTableau2 = new Array();
-
-// Déclaration et initialisation
-const fruits = ["pommes", "mangues", "bananes", "oranges"];
-// Sans boucle
-// console.log(`fruits[0] : ${fruits[0]}`);
-// console.log(`fruits[1] : ${fruits[1]}`);
-// console.log(`fruits[2] : ${fruits[2]}`);
-// console.log(`fruits[3] : ${fruits[3]}`);
-
-// Avec une boucle for
-for (let i = 0; i < fruits.length; i++) {
-  console.log(`fruits[${i}] : ${fruits[i]}`);
+// Addtion 2 nombres
+function addition(nombre1, nombre2) {
+  return nombre1 + nombre2;
 }
 
-fruits.push("citrons");
+const addition2 = function (nombre1, nombre2) {
+  return nombre1 + nombre2;
+};
 
-// Copie de la référence et non du tableau
-const fruits2 = fruits;
-console.log(fruits);
+console.log(addition(5, 6));
+console.log(addition2(11, 12));
 
-// Créer un nouveau tableau à partir d'un tableau existant
-// sans copier la référence
-const fruits3 = [...fruits];
+function calculerLaSommeDuTableau(tableau) {
+  let somme = 0;
+  for (let i = 0; i < tableau.length; i++) {
+    somme += tableau[i];
+  }
+  return somme;
+}
+
+const monTableau = [2, 3, 4, 5, 6];
+console.log(calculerLaSommeDuTableau(monTableau));
